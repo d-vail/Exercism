c8fe109c9464eb5f2bfc36d0e12353f87b4b8794
Danny Vail <danny_vail@outlook.com>
2018-11-01 22:16:24 -0400

    COMPLETE LEAP JS EXERCISE In leap.js - Given a year, determine whether it is a leap year

---
+++ javascript/leap/leap.js
@@ -0,0 +1,16 @@
+/**
+ * @file Exercism: Leap
+ */
+
+/**
+ * Given a year return true if it is a leap year and false if it is a common year
+ *
+ * @returns {boolean}
+ */
+export const isLeap = (year) => {
+  const divBy4 = year % 4 === 0;
+  const divBy100 = year % 100 === 0;
+  const divBy400 = year % 400 === 0;
+
+  return (divBy4 && !divBy100) || (divBy100 && divBy400);
+};
