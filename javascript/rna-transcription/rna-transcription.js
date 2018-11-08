6e688912d03263c1f4a6ef86fbb9d65391673b9b
Danny Vail <danny_vail@outlook.com>
2018-11-07 20:30:45 -0500

    COMPLETE RNA TRANSCRIPTION JS EXERCISE In rna-transcription.js - Given a DNA string, return the complementing RNA sequence

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -2,4 +2,29 @@
  * @file Exercism: RNA Transcription
  */
 
-export const toRna = () => {};
+ /**
+  * DNA to RNA nucleotide complements.
+  * @const {object}
+  */
+const RNA_KEY = {
+  G: 'C',
+  C: 'G',
+  T: 'A',
+  A: 'U',
+};
+
+/**
+ * Given a DNA sequence, return its RNA complement.
+ * @param {string} dna - DNA sequence.
+ * @returns {string} The complementing RNA sequence.
+ */
+export const toRna = (dna) => {
+  let rna = '';
+
+  for (let i = 0; i < dna.length; i += 1) {
+    if (!RNA_KEY[dna[i]]) throw new Error('Invalid input DNA.');
+    rna += RNA_KEY[dna[i]];
+  }
+
+  return rna;
+};
