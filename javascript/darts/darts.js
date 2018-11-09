403bf8d65d0c4ba457874ef81bb5df5da8821a69
Danny Vail <danny_vail@outlook.com>
2018-11-08 22:55:50 -0500

    COMPLETE DARTS JS EXERCISE In darts.js - Given a point representing a single toss in a Darts game, return the earned score

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -2,4 +2,46 @@
  * @file Exercism: Darts
  */
 
-export default () => {};
+/**
+ * Dart target radii and points.
+ * @const {object}
+ */
+const DARTS = {
+  inner: {
+    radius: 1,
+    points: 10,
+  },
+  mid: {
+    radius: 5,
+    points: 5,
+  },
+  outer: {
+    radius: 10,
+    points: 1,
+  },
+};
+
+/**
+ * Given an argument, determine if it is a number.
+ * @param {*} arg
+ * @returns {boolean}
+ */
+const isNaN = arg => typeof arg !== 'number';
+
+/**
+ * Given a point representing a single toss in a Darts game, return the score.
+ * @param {number} x - x coordinate.
+ * @param {number} y - y coordinate.
+ * @returns {number} The points earned.
+ */
+export default (x, y) => {
+  if (isNaN(x) || isNaN(y)) return null;
+
+  const maxPosition = Math.max(x, y);
+
+  if (maxPosition <= DARTS.inner.radius) return DARTS.inner.points;
+  if (maxPosition <= DARTS.mid.radius) return DARTS.mid.points;
+  if (maxPosition <= DARTS.outer.radius) return DARTS.outer.points;
+
+  return 0;
+};
