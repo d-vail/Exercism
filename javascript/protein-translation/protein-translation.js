85f4a22043c520dbf353b940eb3d0624fda2cf5a
Danny Vail <danny_vail@outlook.com>
2018-11-29 23:55:36 -0500

    TRANSLATE RNA TO POLYPEPTIDE In protein-translation.js - Pass tests to translate RNA sequence to polypeptide - Empty string fails - Invalic codon does not throw error

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -1,1 +1,31 @@
-export default () => {};
+const CODON_TO_PROTEIN = {
+  AUG: 'Methionine',
+  UUU: 'Phenylalanine',
+  UUC: 'Phenylalanine',
+  UUA: 'Leucine',
+  UUG: 'Leucine',
+  UCU: 'Serine',
+  UCC: 'Serine',
+  UCA: 'Serine',
+  UCG: 'Serine',
+  UAU: 'Tyrosine',
+  UAC: 'Tyrosine',
+  UGU: 'Cysteine',
+  UGC: 'Cysteine',
+  UGG: 'Tryptophan',
+  UAA: false,
+  UAG: false,
+  UGA: false,
+};
+
+export default rna => rna.match(/(.{3})/g).reduce(
+  (acc, codon) => {
+    if (acc.stop) return acc;
+
+    const protein = CODON_TO_PROTEIN[codon];
+    return protein
+      ? { ...acc, polypeptide: acc.polypeptide.concat([protein]) }
+      : { ...acc, stop: true };
+  },
+  { stop: false, polypeptide: [] },
+).polypeptide;
