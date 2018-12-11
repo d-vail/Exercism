1c52db52ef0fdcfb07929c2b742d0167c42cb373
Danny Vail <danny_vail@outlook.com>
2018-12-10 21:56:17 -0500

    SIMPLE CIPHER: GENERATE A RANDOM KEY In simple-cipher.js - Accept a key during the creation of the cipher - Generate a random key if one is not provided

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -3,7 +3,27 @@
  */
 
 class Cipher {
+  constructor(key = null) {
+    this.key = key || Cipher.generateKey();
+  }
 
+  /**
+   * Generate a random key of 100 lowercase characters in length.
+   * @returns {string} 100 character key
+   */
+  static generateKey() {
+    const key = new Array(100).fill(0);
+    return key.reduce(acc => acc + String.fromCharCode(Cipher.getRandomInt(97, 121)), '');
+  }
+
+  /**
+   * Generate a random integer.
+   * @param {number} min - The minimum integer to allow in range.
+   * @param {number} max - The maximum integer to allow in range.
+   */
+  static getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
 
 export { Cipher };
